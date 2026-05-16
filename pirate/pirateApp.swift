633bdc828986b53734241fc3e44a640490e8ef89
beepboop <hello@email.com>
2026-05-16 08:35:42 +0400

    Initial Commit

---
+++ pirate/pirateApp.swift
@@ -0,0 +1,17 @@
+//
+//  pirateApp.swift
+//  pirate
+//
+//  Created by Guest User on 16.05.26.
+//
+
+import SwiftUI
+
+@main
+struct pirateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
